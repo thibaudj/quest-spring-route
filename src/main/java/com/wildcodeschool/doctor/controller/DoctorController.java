b70104f416c9882eaca3653213771f8010eb21c9
thibaudj <thibaud.jacquemot@sncf.fr>
2020-12-14 16:53:47 +0100

    First commit

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,26 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Doctor getDoctorByNumber(@PathVariable int number) {
+    	if (number == 13) {
+    		return new Doctor(13, "Joddie Whittaker");
+    	} else if (number >= 1 && number <= 12) {
+    		throw new ResponseStatusException(HttpStatus.SEE_OTHER, "See other");
+    	} else {
+    		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+    	}
     }
 }
